b181040e425643aa1470e43b354ba8dc7fd01362
Abhinav <abhinav.vp@hotmail.com>
2026-03-12 13:02:27 +0530

    dist

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/watchlist", watchlistRoute);
 
 // Serve frontend in production
-app.use(express.static(path.join(__dirname, "frontend",)));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
 });
 
 // MongoDB Connection
